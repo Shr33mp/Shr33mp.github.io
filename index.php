a91aebf7b2ef7a69686dad37acfce5e811baf2a2
Shr33mp <mjmateo000@gmail.com>
2024-08-12 02:07:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+    <h1>welcome</h1>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,49 @@
+<html>
+    <head>
+        <title>Sign up</title>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beer-Tual - Products</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+    <form>
+    <!-- name -->
+    <div class="mb-3">
+        <label for="name" class="form-label">Name: </label>
+        <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp" requred value=""> </br>
+    <div class="mb-3">
+
+    <!-- username -->
+    <div class="mb-3">
+        <label for="username" class="form-label">Username: </label>
+        <input type="text" class="form-control" name="username" id="username" aria-describedby="emailHelp" requred value=""> </br>
+    </div>
+    <div class="mb-3"> 
+
+    <!-- email -->
+    <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" name="email" id="name" aria-describedby="emailHelp" requred value="">
+    <div id="emailHelp" class="form-text color:white">We'll never share your email with anyone else.</div>
+  </div>
+
+  <!-- password -->
+  <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" name="email" id="name" aria-describedby="emailHelp" requred value="">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+
+
+
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </body>
+</html>
